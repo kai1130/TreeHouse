096415990b7a7723e0b449d97e05b56a4c8c050f
kai1130 <kaihayden97@gmail.com>
2023-06-21 02:08:56 -0700

    add data

---
+++ options/models.py
@@ -0,0 +1,61 @@
+models = {
+    'Model H':{
+        'name': 'Model H',
+        'price': 40000,
+        'manufacturer': 'Hedera Motors Dallas',
+        'address': '2400 Aviation Dr, DFW Airport, TX 75261, USA',
+        'weight': 1800,
+        'materials':{
+            'metal': 1000,
+            'plastic': 400,
+        },
+        'options': {
+            'wheels': None,
+            'seats': None
+        }
+    },
+    'Model B':{
+        'name': 'Model B',
+        'price': 70000,
+        'manufacturer': 'Hedera Motors Hangzhou',
+        'address': '6CPQ+9HC, Xiaoshan District, Hangzhou, Zhejiang, China',
+        'weight': 1800,
+        'materials':{
+            'metal': 1100,
+            'plastic': 500,
+        },
+        'options': {
+            'wheels': None,
+            'seats': None
+        }
+    },
+    'Model A':{
+        'name': 'Model A',
+        'price': 50000,
+        'manufacturer': 'Hedera Motors Dallas',
+        'address': '2400 Aviation Dr, DFW Airport, TX 75261, USA',
+        'weight': 2100,
+        'materials':{
+            'metal': 1300,
+            'plastic': 700,
+        },
+        'options': {
+            'wheels': None,
+            'seats': None
+        }    },
+    'Model R':{
+        'name': 'Model R',
+        'price': 80000,
+        'manufacturer': 'Hedera Motors Hangzhou',
+        'address': '6CPQ+9HC, Xiaoshan District, Hangzhou, Zhejiang, China',
+        'weight': 2100,
+        'materials':{
+            'metal': 1500,
+            'plastic': 800,
+        },
+        'options': {
+            'wheels': None,
+            'seats': None
+        }
+    }
+}
---
+++ options/seats.py
@@ -0,0 +1,24 @@
+seats = {
+    'Black Seat': {
+        'name': 'Black Seat',
+        'manufacturer': 'Tokyo Seat Company',
+        'price': 0,
+        'address': '1-1 Furugome, Narita, Chiba 282-0004, Japan', # Tokyo Airport
+        'weight': 100, # kg
+        'materials': {
+            'leather': 15,
+            'metal': 20
+        }
+    },
+    'White Seat': {
+        'name': 'White Seat',
+        'manufacturer': 'Mexico City Seat Company',
+        'price': 1000,
+        'address': 'Av. Capitan Carlos Leon S/N, Penon de los Banos, Venustiano Carranza, 15620 Ciudad de México, CDMX, Mexico', # Mexico City Airport
+        'weight': 100, # kg
+        'materials' : {
+            'leatherette': 15,
+            'metal': 20
+        }
+    }
+}
